5cf947c9a72cd7dd569490eff8ebde1191acef75
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-07-19 22:07:10 +0530

    copy constructor qs

---
+++ oppe1pyq/Mock qs 1/Test.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Employee{
+    String name;
+    String[] projects; 
+//***** Define constructor(s) here
+    public Employee(String name,String[] projects){
+        this.name = name;
+        this.projects = projects;
+    }
+    public Employee(Employee e1){
+        this.name = e1.name;
+        int n = e1.projects.length;
+        this.projects = new String[n];
+        for (int i=0;i<n;i++){
+            this.projects[i] = e1.projects[i];
+        }
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+    public void setProject(int index, String proj) {
+        projects[index] = proj;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getProject(int indx) {
+        return projects[indx];
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] proj =  {"PJ1", "PJ2", "PJ3"};
+        Employee e1 = new Employee("Surya", proj);
+        Employee e2 = new Employee(e1);
+        e2.setName(sc.next());
+        e2.setProject(0, sc.next());
+        System.out.println(e1.getName() + ": " + e1.getProject(0));
+        System.out.println(e2.getName() + ": " + e2.getProject(0));
+        sc.close();
+    }
+}
