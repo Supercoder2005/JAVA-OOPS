39f0e8f1b3448b9d5553d14b0f7db200757b6063
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-08-02 23:46:41 +0530

    logger in java

---
+++ Week 7 logger/SimpleLoggerExample.java
@@ -0,0 +1,31 @@
+
+import java.util.logging.*;
+public class SimpleLoggerExample {
+    //get logger for this class
+    private static final Logger logger = Logger.getLogger(SimpleLoggerExample.class.getName());
+    public static void process(){
+        logger.info("Aplication started.");
+        try{
+            int result = divide(10,0); // generate a zero division error
+            logger.info("Result :"+ result);
+        }
+        catch(ArithmeticException e){
+            logger.severe("Exception error :"+e.getMessage());
+        }
+        logger.warning("This is a warning message.");
+        logger.log(Level.INFO,"Application finished.");
+        logger.log(Level.INFO,"Terminated.");
+    }
+    public static void endProcess(){
+        logger.log(Level.WARNING,"This is a warning message");
+        logger.log(Level.INFO,"Application finished");
+    }
+    public static int divide(int a,int b){
+        return a/b;
+    }
+    public static void main(String[] args) {
+        process();
+        logger.setLevel(Level.ALL);
+        endProcess();
+    }
+}
