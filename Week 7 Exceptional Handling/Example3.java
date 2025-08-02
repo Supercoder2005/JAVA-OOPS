699098894887202c58143e15921ec7108749d192
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-08-02 20:39:33 +0530

    use of finally block

---
+++ Week 7 Exceptional Handling/Example3.java
@@ -0,0 +1,19 @@
+// use of finally block 
+public class Example3 {
+    public static void divide(int a, int b){
+        try{
+            System.out.println("Result: "+ (a/b));
+        }
+        catch(ArithmeticException e){
+            System.out.println("Exception caught: "+ e.getMessage());
+        }
+        finally{
+            System.out.println("Inside finally block - Division attempt finished.");
+        }
+    }
+    public static void main(String[] args) {
+        divide(10,2);
+        divide(10,0);
+    }
+    
+}
