0ab2cc4ab91011e31353d5c1cc661982874df132
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-08-17 11:33:08 +0530

    stream example

---
+++ Streams/StreamTest.java
@@ -0,0 +1,40 @@
+package Streams;
+import java.util.*;
+import java.util.stream.*;
+class Product {
+    private String name;
+    private double price;
+    private int stockQuantity;
+    public Product(String n, double p, int sq) {
+        name = n;
+        price = p;
+        stockQuantity = sq;
+    }
+    public String toString() {
+        return name + "- " + price + "- In Stock: " + stockQuantity;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+    // define method productProcessor
+    public static Stream<Product> productProcessor(ArrayList<Product> products){
+        return products.stream().filter(product -> product.getPrice() > 5000.00 && product.getStockQuantity() > 10);
+    }
+}
+ public class StreamTest {
+    public static void main(String[] args) {
+        ArrayList<Product> products = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 4; i++) {
+            Product product = new Product(sc.next(), sc.nextDouble(), sc.nextInt());
+            products.add(product);
+        }
+        Stream<Product> filteredStream = Product.productProcessor(products);
+        System.out.println("Premium Products with Sufficient Stock:");
+        filteredStream.forEach(System.out::println);
+        sc.close();
+    }
+}
